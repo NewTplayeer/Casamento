49db3894dedde920bf343bbdd7d93e8750c50a9b
Bruno <brunotavaresdefreitas@gmail.com>
2026-02-02 23:30:46 -0300

    Funcioando

---
+++ public/index.js
@@ -0,0 +1,11 @@
+// O React foi desativado para permitir que a versão HTML puro (index.html) funcione sem conflitos.
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
